88f3c4cdf1375eaf2fd2fb90317170334e5702d4
l1ttleO <l1ttleofficial@outlook.com>
2021-03-15 00:14:20 +0500

    Initial commit

---
+++ src/main/java/ru/l1ttleO/hideplayercount/HidePlayerCount.java
@@ -0,0 +1,22 @@
+package ru.l1ttleO.hideplayercount;
+
+import com.destroystokyo.paper.event.server.PaperServerListPingEvent;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class HidePlayerCount extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @Override
+    public void onDisable() {}
+
+    @EventHandler
+    public void onServerListPing(PaperServerListPingEvent event) {
+        event.setHidePlayers(true);
+    }
+}
